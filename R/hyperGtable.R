e6fe8e79f4124251230ef2068c345579242f9da5
James MacDonald <jmacdon@med.umich.edu>
2006-02-23 21:41:32 +0000

    JWM: Moved hyperGtable and hyperG2Affy to GOstats package
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/affycoretools@16341 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/hyperGtable.R
+++ R/hyperGtable.R
@@ -14,76 +14,29 @@
 ##  Added hyperG2annaffy 12-12-05
 ##    -This function takes the output of hyperGtable and outputs
 ##     HTML tables from the Affy IDs associated with each GOID
+##
+##  Moved hyperGtable and hyperG2Affy to GOstats 2-23-06
 ###########################################
 
-hyperGtable <- function(probids, lib, type="MF", pvalue=0.05,
-                        min.count=10, save = FALSE, output = TRUE,
-                        filename = NULL){
-  require("GOstats", quietly = TRUE) || stop("The GOstats package is required")
-  require(lib, quietly = TRUE, character.only = TRUE) || stop(paste("The ", lib, " package is required"))
-  lls <- getLL(probids, lib)
-  lls <- unique(lls)
-  lls <- lls[!is.na(lls)]
-  tmp <- GOHyperG(lls, lib, type)
-  index <- tmp$pvalues < pvalue & tmp$goCounts > min.count
-  wh <- mget(names(tmp$pvalues[index]), GOTERM)
-  tmp.terms <- sapply(wh, Term)
-  out <- data.frame(names(tmp$pvalues[index]), tmp.terms, round(tmp$pvalues[index], 3), 
-                       tmp$intCounts[index], tmp$goCounts[index])
-  names(out) <- c("GO Term","Description","p-values",
-                     "Number significant","Number on chip")
-  if(output){
-    if(!is.null(filename)){
-      write.table(out, paste(filename, ".txt", sep=""), sep="\t", quote=FALSE, row.names=FALSE)
-    }else{
-      stop("You need to give a filename in order to output this table!\n")
-    }
-  }
-  if(save)
-    out
-}
 
-hyperG2Affy <- function(probids, lib, type="MF", pvalue=0.05,
-                        min.count=10){
-  require("GOstats", quietly = TRUE) || stop("The GOstats package is required")
-  require(lib, quietly = TRUE, character.only = TRUE) || stop(paste("The ", lib, " package is required"))
-  lls <- getLL(probids, lib)
-  lls <- unique(lls)
-  lls <- lls[!is.na(lls)]
-  tmp <- GOHyperG(lls, lib, type)
-  index <- tmp$pvalues < pvalue & tmp$goCounts > min.count
-  wh <- mget(names(tmp$pvalues[index]), GOTERM)
-  tmp.terms <- sapply(wh, Term)
-  index2 <- match(names(tmp.terms), names(tmp$go2Affy))
-  out <- lapply(tmp$go2Affy[index2], function(x) x[x %in% probids])
-  names(out) <- names(tmp.terms)
-  out
-}
- 
-  
 hyperG2annaffy <- function(probids, lib, eset, fit = NULL, subset = NULL, comp = 1,
-                           type="MF", pvalue = 0.05, min.count = 10){
-  tab <- hyperGtable(probids = probids, lib = lib, type = type, pvalue = pvalue,
-                     min.count = min.count, save = TRUE, output = FALSE)
-  if(!is.null(subset)) tab <- tab[subset,]
-  prbs <- vector("list", dim(tab)[1])
-  for(i in seq(along = prbs)) 
-    prbs[[i]] <- probids[probids %in% get(as.character(tab[i,1]), 
-                                          get(paste(lib, "GO2ALLPROBES", sep = "")))]
-  for(i in seq(along = prbs)){
-    class(prbs[[i]]) <- "character"
-    index <- geneNames(eset) %in% prbs[[i]]
+                           type = "MF", pvalue = 0.05, min.count = 10){
+  tab <- hyperG2Affy(probids, lib, type, pvalue, min.count)
+  if(!is.null(subset))
+    tab <- tab[subset]
+  tab <- lapply(tab, unique)
+  for(i in seq(along = tab)){
     if(!is.null(fit)){
+      index <- tab[[i]] %in% geneNames(eset)
       ord <- order(abs(fit$t)[index], decreasing = TRUE)
-      otherdata <- list("t-statistic" = round(fit$t[index][ord], 2),
-                        "p-value" = round(p.adjust(fit$p.value, "fdr")[index][ord], 3),
-                        "Fold Change" = round(fit$coef[index][ord],2))
-      probes2table(eset, prbs[[i]][ord], annotation(eset), otherdata = otherdata,
-                   filename = paste(as.character(tab[i,2]), "genes", sep=" "))
+      otherdata <- list("t-statistic" = round(fit$t[,comp][index][ord], 2),
+                        "p-value" = round(p.adjust(fit$p.value[,comp], "fdr")[index][ord], 3),
+                        "Fold Change" = round(fit$coef[,comp][index][ord],2))
+      probes2table(eset, tab[[i]][ord], annotation(eset), otherdata = otherdata,
+                   filename = paste(Term(get(names(tab)[i], GOTERM)), "genes", sep=" "))
     }else{
-      probes2table(eset, prbs[[i]], annotation(eset),
-                   filename = paste(as.character(tab[i,2]), "genes", sep = " "))
+      probes2table(eset, tab[[i]], annotation(eset),
+                   filename = paste(Term(get(names(tab)[i], GOTERM)), "genes", sep = " "))
     }
   }
 }
-  
