430a6d7620b4202b8056e0288e5afe5d08d8f956
James MacDonald <jmacdon@med.umich.edu>
2007-07-31 20:03:36 +0000

    Fixed code underlying VennSelect() functions to correctly count comparisons when method='same','sameup', or 'samedown'.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/affycoretools@26080 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/vennSelect.R
+++ R/vennSelect.R
@@ -55,47 +55,47 @@ makeIndices <- function(x, method = "same"){
                    sameup = x, samedown = x))
   if(ncol(x) == 2){
     if(method != "sameup" && method != "samedown"){
-      indices[[1]] <- abs(x[,1]) == 1 & x[,2] == 0
-      indices[[2]] <- x[,1] == 0 & abs(x[,2]) == 1
+      indices[[1]] <- abs(x[,1]) == 1 & x[,2] != x[,1]
+      indices[[2]] <- x[,1] != x[,2] & abs(x[,2]) == 1
       indices[[3]] <- abs(rowSums(x)) == 2
     }
     if(method == "sameup"){
-      indices[[1]] <- x[,1] == 1 & x[,2] == 0
-      indices[[2]] <- x[,1] == 0 & x[,2] == 1
+      indices[[1]] <- x[,1] == 1 & x[,2] != x[,1]
+      indices[[2]] <- x[,1] != x[,2] & x[,2] == 1
       indices[[3]] <- rowSums(x) == 2
     }
     if(method == "samedown"){
-      indices[[1]] <- x[,1] == -1 & x[,2] == 0
-      indices[[2]] <- x[,1] == 0 & x[,2] == -1
+      indices[[1]] <- x[,1] == -1 & x[,2] != x[,1]
+      indices[[2]] <- x[,1] != x[,2] & x[,2] == -1
       indices[[3]] <- rowSums(x) == -2
     }
   }
   if(ncol(x) == 3){
     if(method != "sameup" && method != "samedown"){
-      indices[[1]] <- abs(x[,1]) == 1 & x[,2] == 0 & x[,3] == 0
-      indices[[2]] <- x[,1] == 0 & abs(x[,2]) == 1 & x[,3] == 0
-      indices[[3]] <- x[,1] == 0 & x[,2] == 0 & abs(x[,3]) == 1
-      indices[[4]] <- abs(rowSums(x[,1:2])) == 2 & x[,3] == 0
-      indices[[5]] <- abs(rowSums(x[,c(1,3)])) == 2 & x[,2] == 0
-      indices[[6]] <- abs(rowSums(x[,2:3])) == 2 & x[,1] == 0
+      indices[[1]] <- abs(x[,1]) == 1 & x[,2] != x[,1] & x[,3] != x[,1]
+      indices[[2]] <- x[,1] != x[,2] & abs(x[,2]) == 1 & x[,3] != x[,2]
+      indices[[3]] <- x[,1] != x[,3] & x[,2] != x[,3] & abs(x[,3]) == 1
+      indices[[4]] <- abs(rowSums(x[,1:2])) == 2 & x[,3] != x[,1]
+      indices[[5]] <- abs(rowSums(x[,c(1,3)])) == 2 & x[,2] != x[,1]
+      indices[[6]] <- abs(rowSums(x[,2:3])) == 2 & x[,1] != x[,2]
       indices[[7]] <- abs(rowSums(x)) == 3
     }
     if(method == "sameup"){
-      indices[[1]] <- x[,1] == 1 & x[,2] == 0 & x[,3] == 0
-      indices[[2]] <- x[,1] == 0 & x[,2] == 1 & x[,3] == 0
-      indices[[3]] <- x[,1] == 0 & x[,2] == 0 & x[,3] == 1
-      indices[[4]] <- rowSums(x[,1:2]) == 2 & x[,3] == 0
-      indices[[5]] <- rowSums(x[,c(1,3)]) == 2 & x[,2] == 0
-      indices[[6]] <- rowSums(x[,2:3]) == 2 & x[,1] == 0
+      indices[[1]] <- x[,1] == 1 & x[,2] != x[,1] & x[,3] != x[,1]
+      indices[[2]] <- x[,1] != x[,2] & x[,2] == 1 & x[,3] != x[,2]
+      indices[[3]] <- x[,1] != x[,3] & x[,2] != x[,3] & x[,3] == 1
+      indices[[4]] <- rowSums(x[,1:2]) == 2 & x[,3] != x[,1]
+      indices[[5]] <- rowSums(x[,c(1,3)]) == 2 & x[,2] != x[,1]
+      indices[[6]] <- rowSums(x[,2:3]) == 2 & x[,1] != x[,2]
       indices[[7]] <- rowSums(x) == 3
     }
     if(method == "samedown"){
-      indices[[1]] <- x[,1] == -1 & x[,2] == 0 & x[,3] == 0
-      indices[[2]] <- x[,1] == 0 & x[,2] == -1 & x[,3] == 0
-      indices[[3]] <- x[,1] == 0 & x[,2] == 0 & x[,3] == -1
-      indices[[4]] <- rowSums(x[,1:2]) == -2 & x[,3] == 0
-      indices[[5]] <- rowSums(x[,c(1,3)]) == -2 & x[,2] == 0
-      indices[[6]] <- rowSums(x[,2:3]) == -2 & x[,1] == 0
+      indices[[1]] <- x[,1] == -1 & x[,2] != x[,1] & x[,3] != x[,1]
+      indices[[2]] <- x[,1] != x[,2] & x[,2] == -1 & x[,3] != x[,2]
+      indices[[3]] <- x[,1] != x[,3] & x[,2] != x[,3] & x[,3] == -1
+      indices[[4]] <- rowSums(x[,1:2]) == -2 & x[,3] != x[,1]
+      indices[[5]] <- rowSums(x[,c(1,3)]) == -2 & x[,2] != x[,1]
+      indices[[6]] <- rowSums(x[,2:3]) == -2 & x[,1] != x[,2]
       indices[[7]] <- rowSums(x) == -3
     }
   }
